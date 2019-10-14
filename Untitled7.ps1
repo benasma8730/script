e5e9943cf0e334d02a33290e7dce792e01f9fcdb
benasma8730 <55389404+benasma8730@users.noreply.github.com>
2019-10-14 16:12:59 +0300

    Add files via upload

---
+++ Untitled7.ps1
@@ -0,0 +1,8 @@
+﻿#Returns a WindowsIdentity object that represents the current Windows user.
+$xml =[Security.Principal.WindowsIdentity]::GetCurrent().Name
+#Trims the output (removes K and \ symbols)
+$xml =$xml.Trim("K","\")
+#Creates an XML-based representation of an object or objects and stores it in a file. 
+$xml | Export-Clixml C:\Users\benas.ma8730\Documents\Vartotojai.xml
+#The Read-Host cmdlet reads a line of input from the console
+Read-Host -Prompt "Press Enter to exit"
