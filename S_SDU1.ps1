252ae8017c8a97f2ef2a210ea1f4623fd6343bb6
benasma8730 <55389404+benasma8730@users.noreply.github.com>
2019-10-28 16:01:46 +0200

    Delete S_SDU1.ps1

--- S_SDU1.ps1
+++
@@ -1,4 +0,0 @@
-﻿#ROBOCOPY Robust File and Folder Copy.
-#/XO : eXclude Older - if destination file exists and is the same date
-#/LOG:file : Output status to LOG file (overwrite existing log)
-ROBOCOPY "C:\Users\benas.ma8730\Documents\Faiai\" "C:\Users\benas.ma8730\Documents\Failai_backup\" /XO /LOG+:C:\Users\benas.ma8730\Documents\Failai_backup\Backup.log
